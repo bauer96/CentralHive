300c6f6f393ecce5092ea4f4c9993a38e1a6a64c
Hannes Bauer <zocken210@web.de>
2025-01-21 22:50:16 +0100

    Initial Commit

---
+++ CentralHive/CentralHiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentralHiveApp.swift
+//  CentralHive
+//
+//  Created by Hannes Bauer on 21.01.25.
+//
+
+import SwiftUI
+
+@main
+struct CentralHiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
